ade9a7a99fe15bae99e9efb1396e31b8e816b7a3
Aayan Srivastava <superaayan9@gmail.com>
2022-04-10 22:27:47 +0530

    Stack using Array & Linked List

--- CSES Problem Set/IncreasingArray_CSES.py
+++ CSES Problem Set/IncreasingArray_CSES.py
@@ -1,15 +1,8 @@
 n=int(input())
-l=list(map(int,input().split()))
-c=0
-for i in range(n-1):
-    f=0
-    for j in range(n-1-i):
-        if l[j]>l[j+1]:
-            c+=1
-            t=l[j]
-            l[j]=l[j+1]
-            l[j+1]=t
-            f=1
-    if f==0:
-        break
-print(c)
+nums=list(map(int,input().split()))
+s=0
+for i in range(1,len(nums)):
+    while(nums[i]<=nums[i-1]):
+        s+=1
+        nums[i]+=1
+print(s)
