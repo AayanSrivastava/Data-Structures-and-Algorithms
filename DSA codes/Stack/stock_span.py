a4de478756a3954577fbb2458cfb4e77cf05de0f
Aayan Srivastava <superaayan9@gmail.com>
2022-05-24 23:25:18 +0530

    Stack problems done

--- DSA codes/Stack/stock_span.py
+++ DSA codes/Stack/stock_span.py
@@ -1,9 +1,11 @@
+# a=[100,80,60,70,65,75,80]
+
 class stack:
     def __init__(self):
         self.container=[]
 
-    def push(self,val):
-        self.container.append(val)
+    def push(self,val,x):
+        self.container.append((val,x))
 
     def pop(self):
         return self.container.pop()
@@ -26,21 +28,21 @@ class stack:
     def display(self):
         return self.container 
 
-s=[1,3,2,4]
+s=[100,80,60,70,65,75,80]
 a=[]
 st=stack()
-for i in range(len(s)-1,-1,-1):  #next greater to right
-# for i in range(len(s)):   next greater to left
+# for i in range(len(s)-1,-1,-1):  #next greater to right
+for i in range(len(s)):   #next greater to left
     if st.size()==0:
         a.append(-1)
-    elif st.size()>0 and st.top()>s[i]:
-        a.append(st.top())
-    elif st.size()>0 and st.top()<=s[i]:
-        while st.size()>0 and st.top()<=s[i]:
+    elif st.size()>0 and st.top()[0]>s[i]:
+        a.append(i-st.top()[1])
+    elif st.size()>0 and st.top()[0]<=s[i]:
+        while st.size()>0 and st.top()[0]<=s[i]:
             st.pop()
         if st.size()==0:
             a.append(-1)
         else:
-            a.append(st.top())
-    st.push(s[i])
-print(a[::-1])
+            a.append(i-st.top()[1])
+    st.push(s[i],i)
+print(a)
