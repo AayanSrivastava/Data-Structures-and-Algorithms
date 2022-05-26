60aab4c4181680bd462da3abfdca54ad40974cf9
Aayan Srivastava <superaayan9@gmail.com>
2022-05-26 11:24:23 +0530

    stack problems done

--- DSA codes/Stack/stock_span.py
+++ DSA codes/Stack/stock_span.py
@@ -28,21 +28,21 @@ class stack:
     def display(self):
         return self.container 
 
-s=[100,80,60,70,65,75,80]
+s=[10,6,8,5,11,9]
 a=[]
 st=stack()
-# for i in range(len(s)-1,-1,-1):  #next greater to right
-for i in range(len(s)):   #next greater to left
+for i in range(len(s)-1,-1,-1):  #next greater to right
+# for i in range(len(s)):   #next greater to left
     if st.size()==0:
-        a.append(-1)
+        a.append(0)
     elif st.size()>0 and st.top()[0]>s[i]:
-        a.append(i-st.top()[1])
+        a.append(st.top()[1]-i)
     elif st.size()>0 and st.top()[0]<=s[i]:
         while st.size()>0 and st.top()[0]<=s[i]:
             st.pop()
         if st.size()==0:
-            a.append(-1)
+            a.append(0)
         else:
-            a.append(i-st.top()[1])
+            a.append(st.top()[1]-i)
     st.push(s[i],i)
-print(a)
+print(a[::-1])
