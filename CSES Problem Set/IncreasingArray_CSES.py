13463a5cff273a2f29773cf895eba3fa2727ce37
Aayan Srivastava <superaayan9@gmail.com>
2022-07-09 12:43:17 +0530

    Recursion Practise

--- CSES Problem Set/IncreasingArray_CSES.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-nums=list(map(int,input().split()))
-s=0
-for i in range(1,len(nums)):
-    while(nums[i]<=nums[i-1]):
-        s+=1
-        nums[i]+=1
-print(s)
--- Coding Platforms solved problems python/add2no.py
+++
@@ -1,6 +0,0 @@
-t=123
-t=str(t)
-t=list(t)
-for i in range(len(t)):
-    t[i]=int(t[i])
-print(list(t))
