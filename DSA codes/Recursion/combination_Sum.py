e55cdb1d2e8a660acf077ed991b0acaf76294738
Aayan Srivastava <superaayan9@gmail.com>
2022-07-11 23:11:44 +0530

    Recursion Practice + concepts learned

--- DSA codes/Recursion/combination_Sum.py
+++ DSA codes/Recursion/combination_Sum.py
@@ -1,28 +1,29 @@
 class Solution:
-    def sub1(self,nums,output,index,ans,target):
+    def sub1(self,nums,output,index,ans,target,sum):
         
-        if index>=len(nums):
-            return
-        
-        if target==0:
-            ans.append(output[:])
+        if index==len(nums):
+            if target==sum:
+                if output not in ans:
+                    ans.append(output[:])
             return
 
-            # include
-        if target>=nums[index]:
-            output.append(nums[index])
-            self.sub1(nums,output,index+1,ans,target-nums[index])
-
-            # exclude
-            output.pop()
-        self.sub1(nums,output,index+1,ans,target)
+        # include
+        output.append(nums[index])
+        sum+=nums[index]
+        self.sub1(nums,output,index+1,ans,target,sum)
+            
+        output.pop()
+        sum-=nums[index]
+        self.sub1(nums,output,index+1,ans,target,sum)
             
     def combinationSum(self, candidates,target):
         output=[]
         index=0
         ans1=[]
-        self.sub1(candidates,output,index,ans1,target)
+        sum=0
+        candidates.sort()
+        self.sub1(candidates,output,index,ans1,target,sum)
         return ans1
 l1=Solution()
-s=[10,1,2,7,6,1,5]
-print(l1.combinationSum(s,8))
+s=[2,5,2,1,2]
+print(l1.combinationSum(s,5))
