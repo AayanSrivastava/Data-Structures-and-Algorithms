c5aecff43deb57d7fd194d78b273e723d84cefaa
Aayan Srivastava <superaayan9@gmail.com>
2022-07-19 23:02:02 +0530

    Binary Search Concepts

--- DSA codes/Binary Search/first and last occurence in sorted array.py
+++ DSA codes/Binary Search/first and last occurence in sorted array.py
@@ -1,33 +1,18 @@
 class Solution:
     def Bsearchf(self,nums,t,start,end,first):
-        if start>end:
-            return first
-
-        mid=start+(end-start)//2
-        if nums[mid]==t:
-            first=mid
-            return self.Bsearchf(nums,t,start,mid-1,first)
-        if nums[mid]>t:
-            return self.Bsearchf(nums,t,start,mid-1,first)
-        else:
-            return self.Bsearchf(nums,t,mid+1,end,first)
-
-    def Bsearchl(self,nums,t,start,end,last):
-        if start>end:
-            return last
-
-        mid=start+(end-start)//2
-        if nums[mid]==t:
-            last=mid
-            return self.Bsearchl(nums,t,mid+1,end,last)
-        if nums[mid]>t:
-            return self.Bsearchl(nums,t,start,mid-1,last)
-        else:
-            return self.Bsearchl(nums,t,mid+1,end,last)
+        while start<=end:
+            mid=start+(end-start)//2
+            if nums[mid]==t:
+                first=mid
+                return self.Bsearchf(nums,t,start,mid-1,first)
+            if nums[mid]>t:
+                return self.Bsearchf(nums,t,start,mid-1,first)
+            else:
+                return self.Bsearchf(nums,t,mid+1,end,first)
+        return first
+            
 l=Solution()
-ar=[1,3,3,4,5]
+ar=[3,3,3,4,5]
 # ar=[7,6,5,4,3,2,1]  # reverse sorted array
 first=-1
-last=-1
 print(l.Bsearchf(ar,3,0,len(ar)-1,first))
-print(l.Bsearchl(ar,3,0,len(ar)-1,last))
