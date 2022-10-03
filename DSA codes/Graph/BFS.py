97a2f19f18bfa6d7d9f1b00308dc020775d0cac3
Aayan Srivastava <superaayan9@gmail.com>
2022-10-04 01:07:02 +0530

    BFS and DFS implemented

--- DSA codes/Graph/BFS.py
+++ DSA codes/Graph/BFS.py
@@ -1,22 +1,21 @@
 from collections import deque,defaultdict
 
 #Adjacency list
-def create_adj_list_dir(edges):
+def create_adj_list(edges):
     adj=defaultdict(list)
     for e in edges:
         adj[e[0]].append(e[1])
         adj[e[1]].append(e[0])
     return adj
 
-# adj_list_dir=create_adj_list_dir([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
-adj_list_dir=create_adj_list_dir([[0,4]])
-print(adj_list_dir)
-for i in adj_list_dir:
-    print(adj_list_dir[i])
-# print(max(adj_list_dir))
+adj_list=create_adj_list([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
+# print(adj_list)
+# for i in adj_list:
+#     print(adj_list[i])
+# print(max(adj_list))
 
 #Adjacency matrix
-def create_adj(edges):
+def create_adj_mat(edges):
     no_of_nodes=1+ max([e[0] for e in edges] + [e[1] for e in edges])
     adj=[[0]*no_of_nodes for i in range(no_of_nodes)]
     for e in edges:
@@ -24,15 +23,15 @@ def create_adj(edges):
         adj[e[1]][e[0]]=1
     return adj
 
-adj_mat_undir=create_adj([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
+adj_mat=create_adj_mat([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
 # print(adj_mat_undir)
 # print(len(adj_mat_undir))
 
 def bfs(adj):
     print("BFS")
     vis=set()
-    # n=len(adj)  #Adjacency matrix
-    n=max(adj)+1 #Adjacency list
+    n=len(adj)  #Adjacency matrix
+    # n=max(adj)+1 #Adjacency list
     for i in range(n):
         if i in vis:
             continue
@@ -44,7 +43,8 @@ def bfs(adj):
             print(node,end=" ")
             
             for adjnode in range(n):
-                if adjnode in adj[node] and adjnode not in vis:
+                if adj[node][adjnode] and adjnode not in vis:  #Adjacency matrix
+                # if adjnode in adj[node] and adjnode not in vis:  #Adjacency list
                     vis.add(adjnode)
                     q.appendleft(adjnode)
 
@@ -56,5 +56,5 @@ def bfs(adj):
         # if returning list return here(indentation)
     print()
 
-# bfs(adj_mat_undir)
-# bfs(adj_list_dir)
+bfs(adj_mat)
+# bfs(adj_list)
---
+++ DSA codes/Graph/Recursive_dfs.py
@@ -0,0 +1,53 @@
+from collections import deque,defaultdict
+
+#Adjacency list
+def create_adj_list(edges):
+    adj=defaultdict(list)
+    for e in edges:
+        adj[e[0]].append(e[1])
+        adj[e[1]].append(e[0])
+    return adj
+
+adj_list=create_adj_list([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
+# adj_list=create_adj_list([[0,1],[0,2],[0,3],[1,3],[2,4],[3,5],[3,6],[4,7]])
+
+#Adjacency matrix
+def create_adj_mat(edges):
+    no_of_nodes=1+ max([e[0] for e in edges] + [e[1] for e in edges])
+    adj=[[0]*no_of_nodes for i in range(no_of_nodes)]
+    for e in edges:
+        adj[e[0]][e[1]]=1   #directed graph
+        adj[e[1]][e[0]]=1
+    return adj
+
+adj_mat=create_adj_mat([[0,1],[1,2],[1,3],[2,4],[3,4],[5,7],[7,6]])
+# adj_mat=create_adj_mat([[0,1],[0,2],[0,3],[1,3],[2,4],[3,5],[3,6],[4,7]])
+
+
+def dfsm(node,vis,adj,ans):
+    vis.add(node)
+    ans.append(node)
+    n=len(adj)
+    for adjnode in range(n):
+        if adj[node][adjnode] and adjnode not in vis:
+            dfsm(adjnode,vis,adj,ans)
+
+def dfsl(node,vis,adj,ans):
+    vis.add(node)
+    ans.append(node)
+    for adjnode in adj[node]:
+        if adjnode not in vis:
+            dfsl(adjnode,vis,adj,ans)
+
+def dfsofGraph(adj):
+    ans=[]
+    vis=set()
+    n=max(adj)+1
+    # n=len(adj)
+    for i in range(n):
+        if i not in vis:
+            dfsl(i,vis,adj,ans)
+    return ans
+
+# print(dfsofGraph(adj_mat))
+print(dfsofGraph(adj_list))
