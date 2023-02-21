270949ef2bf0ee822c17a7bd8e7f2b3a7ec6adf1
Aayan Srivastava <superaayan9@gmail.com>
2023-02-21 14:37:21 +0530

    DP on subsequences/subsets done

--- DSA codes/Dynamic Programming/partition_equal_subset_sum.py
+++ DSA codes/Dynamic Programming/partition_equal_subset_sum.py
@@ -1,28 +1,10 @@
 class Solution:
-
     #MEMOIZATION
-    def subset(self,arr,ind,target,dp):
-        if ind==len(arr):
-            if target==0:
-                return True
-            return False
-        
-        if dp[ind][target]!=-1:
-            return dp[ind][target]
-            
-        nottake=self.subset(arr,ind+1,target,dp)
-        take=False
-        if target>=arr[ind]:
-            take=self.subset(arr,ind+1,target-arr[ind],dp)
-        dp[ind][target]= take or nottake
-        return dp[ind][target]
-    
-    #from N-1
     def subset(self,arr,ind,target,dp):
         if target==0:
             return True
         if ind==len(arr):
-            return 0
+            return False
         
         if dp[ind][target]!=-1:
             return dp[ind][target]
@@ -39,7 +21,8 @@ class Solution:
         dp=[[0]*(target+1) for i in range(len(arr)+1)]
         for i in range(len(arr)):
             dp[i][0]=True
-        dp[0][arr[0]]=True
+        if target>=arr[0]:
+            dp[0][arr[0]]=True
         for i in range(1,len(arr)):
             for t in range(1,target+1):
                 nottake=dp[i-1][t]
@@ -48,7 +31,7 @@ class Solution:
                     take=dp[i-1][t-arr[i]]
                 dp[i][t]=take or nottake
         return dp[len(arr)-1][target]
-        
+
     #SPACE_OPTIMIZATION
     def subsetOpt(self,arr,target):
         prev=[0]*(target+1)
@@ -65,10 +48,14 @@ class Solution:
                 cur[j]=take or nottake
             prev=cur
         return prev[target]
-        
-    def isSubsetSum (self, N, arr, sum):
-        # dp=[[-1]*(sum+1) for i in range(N+1)]
-        # return self.subset(arr,0,sum,dp)
-        # return self.subset(arr,N-1,sum,dp)
-        # return self.subsetTab(arr,sum)
-        return self.subsetOpt(arr,sum)
+
+    def canPartition(self, nums):
+        totsum=0
+        for i in nums:
+            totsum+=i
+        if totsum%2!=0:
+            return False
+        target=totsum//2
+        dp=[[-1]*(target+1) for i in range(len(nums)+1)]
+        return self.subset(nums,0,target,dp)
+        # return self.subset(nums,target)
