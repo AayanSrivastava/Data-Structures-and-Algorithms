4453a9f1e664d82f1df54d3b3485af7c04d22bc9
superaayan <superaayan9@gmail.com>
2025-05-13 01:37:13 +0530

    space optimized

---
+++ DSA codes/Recursion and Backtracking/Permutations_2.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def solve(self,nums,ans,index):
+        if index==len(nums):
+            ans.append(nums[:])
+            return
+        
+        seen = set()
+        for i in range(index,len(nums)):
+            if nums[i] in seen:
+                continue
+            seen.add(nums[i])
+            nums[index],nums[i]=nums[i],nums[index]
+            self.solve(nums,ans,index+1)
+            nums[index],nums[i]=nums[i],nums[index]
+        
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        self.solve(nums,ans,0)
+        return ans
+
+obj = Solution()
+nums=[1,1,2]
+print(obj.permuteUnique(nums))
--- DSA codes/Recursion and Backtracking/permutation_of_array.py
+++ DSA codes/Recursion and Backtracking/permutation_of_array.py
@@ -4,7 +4,11 @@ class Solution:
             ans.append(S[:])
             return
         
+        seen= set()
         for i in range(index,len(S)):
+            if S[i] in seen:
+                continue
+            seen.add(S[i])
             S[index],S[i]=S[i],S[index]
             self.solve(S,ans,index+1)
             # backtracking
@@ -14,7 +18,7 @@ class Solution:
         ans=[]
         index=0
         self.solve(S,ans,index)
-        return ans
+        return sorted(ans)
 
 l=Solution()
 s=[1,2,3]
