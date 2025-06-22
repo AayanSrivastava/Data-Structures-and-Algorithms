2c99cbbd00db70c9b8f9e4bcbaff2351aef076f1
Aayan Srivastava <superaayan9@gmail.com>
2025-06-23 01:43:11 +0530

    variable size sliding window

---
+++ DSA codes/Sliding Window/minimum_size_subarray_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        i = 0
+        window_sum = 0
+        mini = float('inf')
+        for j in range(len(nums)):
+            window_sum+=nums[j]
+
+            while window_sum>=target:
+                mini = min(mini, j-i+1)
+                window_sum-=nums[i]
+                i+=1
+        
+        return mini if mini!= float('inf') else 0
