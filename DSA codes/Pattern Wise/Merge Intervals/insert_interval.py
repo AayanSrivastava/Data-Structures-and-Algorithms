e62d75e98809d413e5a1da748756ad70a9d8606a
Aayan Srivastava <superaayan9@gmail.com>
2026-04-19 20:41:29 +0530

    Insert interval

---
+++ DSA codes/Pattern Wise/Merge Intervals/insert_interval.py
@@ -0,0 +1,12 @@
+class Solution:
+    def insert(self, intervals, newInterval):
+        intervals.append(newInterval)
+        intervals.sort()
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+            
+        return merged
